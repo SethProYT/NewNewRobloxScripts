38de8fe342d68e5aa16fa5eea2fc5d541c2307c7
SethYT <unknowingman211@gmail.com>
2024-03-29 17:29:07 +0800

    Removed keybind bc no work

--- Landmine Comprimiser.lua
+++ Landmine Comprimiser.lua
@@ -1,11 +1,7 @@
 local mines = game:GetService("Workspace").Map.Obstacles.Minefield.Mines
-local toggle = game:GetService("UserInputService")
-local yesorno = true
-
 
 
 while wait() do
-    if not yesorno == true then return end
     for i,v in pairs(mines:GetDescendants()) do
         if v.Name == "Base" and "Button" then
             v.Transparency = 0
@@ -13,14 +9,3 @@ while wait() do
     end
 end
 
-while wait() do
-    toggle.InputBegan:Connect(function(input)
-        if input.KeyCode == Enum.KeyCode.L then
-            if yesorno == false then
-                yesorno = true
-            elseif yesorno == true then
-                yesorno = false
-            end
-        end
-    end)
-end
