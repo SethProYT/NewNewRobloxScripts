5e35cb10fe3dad2ac3f2e6de370abe605f50bccd
SethYT <unknowingman211@gmail.com>
2024-03-31 20:13:09 +0800

    made anti freeze actually anti freeze - working on new script

--- Anti Freeze.lua
+++ Anti Freeze.lua
@@ -1,9 +1,11 @@
 local plr = game:GetService("Players").LocalPlayer
 local char = plr.Character
 
-for i,v in pairs(char:GetChildren()) do
-    if v.ClassName == "Part" then
-        v.Anchored = false
+while wait() do
+    for i,v in pairs(char:GetChildren()) do
+        if v.ClassName == "Part" then
+            v.Anchored = false
+        end
     end
 end
 -- womp womp
---
+++ Escape Sakura lol.lua
@@ -0,0 +1,8 @@
+-- game id 6851155308
+    -- LOCATIONS ILL USE:
+    -- workspace.Map2
+    -- workspace.Map1
+-- unfinished script btw..
+
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+
